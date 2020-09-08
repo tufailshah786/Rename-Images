e27119bdd574c93a8afdf64c5b753349eb5302ed
Tufail Sajjad Shah Hashmi <69716088+tufailshah786@users.noreply.github.com>
2020-09-08 15:56:26 +0500

    Update rename_img.py

--- rename_img.py
+++ rename_img.py
@@ -4,17 +4,17 @@ import math
 import os
 import xml.etree.ElementTree as ET
 i=1
-for filename in os.listdir("C:/python/rename_images/"): 
+for filename in os.listdir("C:/Rename-Images-master/rename_images/"): 
      filen=os.path.splitext(os.path.basename(filename))[0]
      print(filen)
      extension=os.path.splitext(os.path.basename(filename))[1]
      print(extension)
      if extension=='.jpg':
-       img = cv2.imread("C:/python/rename_images/"+filen + ".jpg")
-       cv2.imwrite("C:/python/rename_images/"+"W"+str(i)+ ".jpg",img)
+       img = cv2.imread("C:/Rename-Images-master/rename_images/"+filen + ".jpg")
+       cv2.imwrite("C:/Rename-Images-master/rename_images/"+"W"+str(i)+ ".jpg",img)
        filen1=("W"+str(i))
        print(filen1)
        if filen!=filen1:
-         os.remove("C:/python/rename_images/"+filen + ".jpg")
+         os.remove("C:/Rename-Images-master/rename_images/"+filen + ".jpg")
        i+=1
        
